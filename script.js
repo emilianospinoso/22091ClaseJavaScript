0cff18f786584f6d3045568ea108ae73fba9c117
emilianospinoso <emilianospinoso@gmail.com>
2022-04-13 20:13:41 -0300

    primera version

---
+++ script.js
@@ -0,0 +1,59 @@
+//alert("Hola Mundo")
+
+/*esto
+es 
+comentario
+*/
+
+//console.log("Hola Consola")
+
+let alumna 
+
+alumna = "pablo"
+
+let edad = 21
+
+console.log(  "La alumna que ingreso es " +  alumna + "y la edad es " + edad);
+
+//Buenas practicas
+let nombreApellidoDelTitular = "jeimu paurl" //camelCase
+let nombre_apellido ="juan carlos"//snakeCase
+let NombreApellido = "fede suarez" //pascalCase
+let $nombreApellido
+let _nombreApellido 
+
+//no vannnnnnnnnnnnnnnnnnnnnnn
+/*
+let nombre-apellido = "juan carlos" //kebab case
+let 1erNombre 
+let var
+*/
+
+//////////////TIPOS DE DATOS/////////////////////////
+/////////STRING
+
+let str1 = `pablo`
+let str2 = "maria"
+let str3 = `otro string mas`
+
+let str4 = str1 + " y " +str2 + " se casan " 
+let str5 = `${str1}  y ${str2} se casan     `
+console.log(str4)
+console.log(str5)
+
+
+
+///////////////NUMBER
+let numero1 =  parseFloat(prompt("mete el primer numero"))
+let numero2 = parseFloat(prompt("mete el sgundo numero"))
+
+let suma = numero1 + numero2 
+let resta= numero1 - numero2
+let multi = numero1 * numero2
+let division =  numero1/numero2
+
+console.log("La suma es " + suma)
+console.log("La resta es " + resta)
+console.log("la mutli es " + multi)
+console.log("la division es " + division)
+
